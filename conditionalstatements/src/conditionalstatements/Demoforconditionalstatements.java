c0af8293e3b4cecf0d89d32eedcf06bda805487c
ADDETLA KAVYA <ADDETLA KAVYA@DESKTOP-QE0DR8L>
2025-08-15 16:10:35 +0530

    Demo for conditional statements

---
+++ conditionalstatements/src/conditionalstatements/Demoforconditionalstatements.java
@@ -0,0 +1,13 @@
+package conditionalstatements;
+
+public class Demoforconditionalstatements {
+
+	public static void main(String[] args) {
+		int x = 20;
+		if(x>0) {
+			System.out.println("given number is positive");
+			
+		}
+	}
+
+}
