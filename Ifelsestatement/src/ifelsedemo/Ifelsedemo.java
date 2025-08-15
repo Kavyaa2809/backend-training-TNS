d070066e91cd83e5aaf358416b1d7b5b5313befc
ADDETLA KAVYA <ADDETLA KAVYA@DESKTOP-QE0DR8L>
2025-08-15 17:04:54 +0530

    if statement...

---
+++ Ifelsestatement/src/ifelsedemo/Ifelsedemo.java
@@ -0,0 +1,16 @@
+package ifelsedemo;
+
+public class Ifelsedemo {
+
+	public static void main(String[] args) {
+		int age = 20;
+		if (age>=18)
+			System.out.println("your eligible for voting");
+		else
+		System.out.println("sorry!you have to wait");	
+		System.out.println("if else demonstaration");
+	
+
+	}
+
+}
---
+++ conditionalstatements/src/conditionalstatements/Democonditionalstatements.java
@@ -0,0 +1,21 @@
+package conditionalstatements;
+
+public class Democonditionalstatements {
+
+    public static void main(String[] args) {
+        int x = 20;
+
+        if (x > 0) {
+            System.out.println("The given number is positive");
+        } 
+        else if (x < 0) {
+            System.out.println("The given number is negative");
+        } 
+        else {
+            System.out.println("The given number is zero");
+        }
+    }
+}
+
+
+
