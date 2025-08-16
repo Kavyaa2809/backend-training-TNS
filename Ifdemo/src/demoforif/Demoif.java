ef95a05c617b6f51ec572fd474b8c65f206c235e
ADDETLA KAVYA <ADDETLA KAVYA@DESKTOP-QE0DR8L>
2025-08-16 17:20:30 +0530

    demo for if statement

---
+++ Ifdemo/src/demoforif/Demoif.java
@@ -0,0 +1,17 @@
+package demoforif;
+
+public class Demoif {
+
+	public static void main(String[] args) {
+		int x = 5;
+		if (x != 5)
+			System.out.println("Value of x is not 5");
+		System.out.println("Value of x is 5");
+	}
+
+
+
+
+	}
+
+
